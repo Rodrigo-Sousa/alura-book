1f92965a6c8b2226c60b1a46e3562f189e13ebeb
Rodrigo Sousa - Work <rodrigo.sdeveloper@gmail.com>
2025-08-18 11:51:57 -0300

    Adicionado o primeiro componente, o logo dentro do header estilizado

--- alura-book-projeto/src/App.js
+++ alura-book-projeto/src/App.js
@@ -1,22 +1,17 @@
-import logo from './logo.svg';
+// Importação do arquivo CSS
 import './App.css';
+import logo from './images/logo.svg';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Alura
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    // Adicionado classes aos componentes para executar o CSS
+    <div className='App'>
+      <header className='App-header'>
+        <logo className='logo'>
+          <img src={logo} alt='Um símbolo de um triângulo laranja, representando a letra A da marca: Alura Book'></img>
+          <p><strong>Alura</strong> Books</p>
+        </logo>
+
       </header>
     </div>
   );
