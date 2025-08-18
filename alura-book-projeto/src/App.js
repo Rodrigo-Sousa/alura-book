a349ba76a34c55fa86b6b7204671437981eb428c
Rodrigo Sousa - Work <rodrigo.sdeveloper@gmail.com>
2025-08-18 15:42:28 -0300

    Adicionado o componente do Logo, para estilizarmos e deixarmos nosso desenvolvimento melhor organizado

--- alura-book-projeto/src/App.js
+++ alura-book-projeto/src/App.js
@@ -1,16 +1,14 @@
 // Importação do arquivo CSS
 import './App.css';
-import logo from './images/logo.svg';
+import Logo from "./components/Logo"
 
 function App() {
   return (
     // Adicionado classes aos componentes para executar o CSS
     <div className='App'>
       <header className='App-header'>
-        <logo className='logo'>
-          <img src={logo} alt='Um símbolo de um triângulo laranja, representando a letra A da marca: Alura Book'></img>
-          <p><strong>Alura</strong> Books</p>
-        </logo>
+        {/* Importando o componente Logo */}
+        <Logo></Logo>
 
       </header>
     </div>
