836484e7b898e4a2f510410fcee929eefc820eb7
Rodrigo Sousa - Work <rodrigo.sdeveloper@gmail.com>
2025-08-22 11:24:49 -0300

    Adicionando containers para utilização, deixando o conteúdo da aplicação em javascript. Feito nos arquivos App.js e no index.js que está no componentes Header

--- alura-book-projeto/src/App.js
+++ alura-book-projeto/src/App.js
@@ -1,14 +1,27 @@
-// Importação do arquivo CSS
-import './App.css';
 import Header from "./components/Header"
+// Importando o Styled-components
+import styled from "styled-components"
+
+// Utilizando a stilização do styled-components. Pacote que "guarda os estilos"
+const AppContainer = styled.div`
+    /* Largura e altura em 100 */
+    width: 100vw;
+    height: 100vh;
+    /* Cor de fundo */
+    background-image: linear-gradient(90deg, #002F52 35%, #326589);
+/* Estilizando todos os li da página */
+li {
+  list-style: none;
+}
+  `
 
 function App() {
   return (
     // Adicionado classes aos componentes para executar o CSS
-    <div className='App'>
+    <AppContainer>
       {/* Utilizando o componente Header */}
       <Header />
-    </div>
+    </AppContainer>
   );
 }
 
--- alura-book-projeto/src/components/Header/index.js
+++ alura-book-projeto/src/components/Header/index.js
@@ -1,18 +1,28 @@
-import "./estilo.css";
 import Logo from "../Logo";
 import OpcoesHeader from "../OpcoesHeader";
 import IconesHeader from "../IconesHeader";
 
+// Utilizando styled-components
+import styled from "styled-components"
+
+// Container do header, possuindo a estilização junto do pacote styled-components
+const HeaderContainer = styled.header`
+/* Estilizando o header que será exibido na página */
+  background-color: #FFF;
+  display: flex;
+  justify-content: center;
+`
+
 function Header(){
     return (
-        <header className='App-header'>
+        <HeaderContainer>
         {/* Como não temos conteúdo dentro destas tags, elas possuem o 'auto fechamento' */}
         {/* Importando o componente Logo */}
         <Logo />
         {/* Importado a lista contendo as opções do Header */}
         <OpcoesHeader />
         <IconesHeader />
-      </header>
+      </HeaderContainer>
     )
 }
 
