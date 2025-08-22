67650e5d6e6b0d4c10f3b3d6e502dc986aa8bff8
Rodrigo Sousa - Work <rodrigo.sdeveloper@gmail.com>
2025-08-22 13:31:44 -0300

    Ajustado o conteúdo do comentário da linha 20 no arquivo App.js

--- alura-book-projeto/src/App.js
+++ alura-book-projeto/src/App.js
@@ -4,20 +4,20 @@ import styled from "styled-components"
 
 // Utilizando a stilização do styled-components. Pacote que "guarda os estilos"
 const AppContainer = styled.div`
-    /* Largura e altura em 100 */
+  /* Largura e altura em 100 */
     width: 100vw;
     height: 100vh;
-    /* Cor de fundo */
+  /* Cor de fundo */
     background-image: linear-gradient(90deg, #002F52 35%, #326589);
-/* Estilizando todos os li da página */
-li {
-  list-style: none;
-}
+  /* Estilizando todos os li da página */
+    li {
+      list-style: none;
+    }
   `
 
 function App() {
   return (
-    // Adicionado classes aos componentes para executar o CSS
+    // Utilizando o componente estilizado
     <AppContainer>
       {/* Utilizando o componente Header */}
       <Header />
