af84aec05ba40a90412a55c1ee3f2fedefd41b8d
Rodrigo Sousa - Work <rodrigo.sdeveloper@gmail.com>
2025-08-26 11:46:09 -0300

    Renderizado na página, os conteúdos dos dadosPesquisa, se a pesquisa encontrar parte de um título que contém na base de dados, ele lista o livro, com a imagem e o título

---
+++ alura-book-projeto/src/components/Pesquisa/dadosPesquisa.js
@@ -0,0 +1,13 @@
+// Importando a imagem para utilização no array
+import livroLiderancaDesign from "../../images/liderancaDesign.png"
+import livroJavascriptAssertivo from "../../images/javascriptAssertivo.png"
+import livroCangaceiroJavascript from "../../images/cangaceiroJavascript.png"
+import livroDesbravandoSOLID from "../../images/desbravandoSOLID.png"
+
+// Simulando um Banco de Dados
+export const livros = [
+    {nome: 'Liderança em Design', id: 1, src: livroLiderancaDesign},
+    {nome: 'JavaScript Assertivo', id: 2, src:livroJavascriptAssertivo},
+    {nome: 'Cangaceiro em JavaScript', id: 3, src:livroCangaceiroJavascript},
+    {nome: 'Desbravando SOLID', id: 4, src:livroDesbravandoSOLID},
+]
--- alura-book-projeto/src/components/Pesquisa/index.js
+++ alura-book-projeto/src/components/Pesquisa/index.js
@@ -4,6 +4,8 @@ import Input from "../Input";
 import styled from "styled-components";
 // Importando o status, 
 import { useState } from "react";
+// Importando o componente de livros
+import { livros } from "./dadosPesquisa";
 
 const PesquisaContainer = styled.section`
     background-image: linear-gradient(90deg, #002F52 35%, #326589 165%);
@@ -26,20 +28,54 @@ const Subtitulo = styled.h3`
     font-weight: 500;
     margin-bottom: 40px;
 `
-
+const Resultado = styled.div`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    margin-bottom: 20px;
+    cursor: pointer;
+    p {
+        width: 200px;
+    }
+    img {
+        width: 100px;
+    }
+    &:hover {
+        border: 1px solid white;
+    }
+`
 // Componente de pesquisa
 function Pesquisa() {
     // Utilizando o status e a função que "muda os valores/estado";
-    const [textoDigitado, setTextoDigitado] = useState('');
+    const [livrosPesquisados, setLivrosPesquisados] = useState([]);
     return (
         <PesquisaContainer>
             <Titulo>Já sabe por onde começar?</Titulo>
             <Subtitulo>Encontre seu livro em nossa estante.</Subtitulo>
             <Input placeholder="Escreva sua próxima leitura"
-            // Lidando com o evento que for passado para a função
-            onBlur={evento => setTextoDigitado(evento.target.value)}
-             />
-             <p>{textoDigitado}</p>
+                // Lidando com o evento que for passado para a função
+                onBlur={evento => {
+                    // Criando um filtro pegando o texto digitado pelo usuário fazendo uma pesquisa na lista de livros, observando os livros que se encaixam na busca
+                    const textDigitado = evento.target.value;
+                    // Essa função é responsável por filtrar os livros digitados na barra de pesquisa.
+                    const resultadoPesquisa = livros.filter(livro => livro.nome.includes(textDigitado));
+                    setLivrosPesquisados(resultadoPesquisa);
+                }
+                }
+            />
+            {
+                // incluindo o .map 
+                livrosPesquisados.map(
+                    // Como parâmetros temos uma arrow function
+                    livro => (
+                        <Resultado>
+                            <img src={livro.src} />
+                            <p>{livro.nome}</p>
+                        </Resultado>
+
+                    )
+                )
+            }
         </PesquisaContainer>
     )
 }
