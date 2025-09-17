f08e21ccb114cd8c63b6a87b17671484cb604738
Rodrigo Sousa - Work <rodrigo.sdeveloper@gmail.com>
2025-09-17 09:42:27 -0300

    Criado o serviço para consumir a API com os dados vindos do Backend

--- alura-book-projeto/src/services/livros.js
+++ alura-book-projeto/src/services/livros.js
@@ -3,3 +3,18 @@ import axios from "axios";
 
 // Conexão com a API
 const livrosAPI = axios.create({baseURL: "http://localhost:8000/livros"});
+
+// Realizando os métodos e funções, para consumir a API do backend 
+
+function getLivros(){
+    // Utilizando o axios.create temos algumas instâncias de retorno, dentro delas, o GET
+    const response = livrosAPI.get('/');
+
+    // Retorno do axios em data, como temos o retorno na variável response, iremos passar ela, com os dados
+    return response.data;
+}
+
+// Exportando as funções
+export {
+    getLivros
+}
